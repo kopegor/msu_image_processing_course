49cebf9d0c4218dbed910ff0474422655df4f9e1
egor.koptelov <egorkoptelov99@gmail.com>
2024-11-25 13:50:19 +0300

    Updated game maze

--- game_maze/maze.py
+++ game_maze/maze.py
@@ -3,101 +3,162 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from time import time
 
+
 def load_resources():
     background = cv.imread('./resource/background.jpg')
     maze = cv.imread('./resource/maze.jpg')
-    return background, maze
+    player = cv.imread('./resource/player.jpg')
+    return background, maze, player
 
 
-def init_game(background, maze):
+def init_game(background, maze, player):
     state = background.copy()
-    state[91:659, 120:1125, :] = maze
-    cur_state = [140, 160]
-    state[cur_state[0]:cur_state[0]+10, cur_state[1]:cur_state[1]+10] = [255, 0, 0]
+    state[100:846, 150:1406, :] = maze
+    cur_state = [170, 220]
     
-    background[91:659, 120:1125, :] = maze
-    
+    background[100:846, 150:1406, :] = maze
+
     return state, cur_state, background
 
 
-def is_failed(cur_state, background, start_time):
+BACKGROUND, MAZE, PLAYER = load_resources()
+STATE, CUR_STATE, BACKGROUND = init_game(BACKGROUND, MAZE, PLAYER)
+
+
+def draw_player():
+    for i in range(PLAYER.shape[0]):
+        for j in range(PLAYER.shape[1]):
+            if PLAYER[i, j][0] < 200 or PLAYER[i, j][1] < 200 or PLAYER[i, j][2] < 200:
+            # if sum(PLAYER[i, j]) < 600:
+                STATE[CUR_STATE[0] - 25 + i, CUR_STATE[1] - 25 + j] = PLAYER[i, j]
+
+draw_player()
+
+def is_failed(
+        cur_state,
+        background, 
+        start_time
+):
     game_time = time() - start_time
-    if ((background[cur_state[0]+5, cur_state[1]+5, 0] ==  254) and (background[cur_state[0]+5, cur_state[1]+5, 1] ==  254) and (background[cur_state[0]+5, cur_state[1]+5, 2] ==  254)):
-        return True
-    elif ((background[cur_state[0]+5, cur_state[1]+5, 0] < 28) and (background[cur_state[0]+5, cur_state[1]+5, 1] < 5) and (background[cur_state[0]+5, cur_state[1]+5, 2] >130)):
-        return True
-    elif sum(background[cur_state[0]+5, cur_state[1]+5]) < 20:
-        return True
-    elif    game_time > 60:
-        return True 
-    return False
+    failed_area = 15
+    for i in range(failed_area):
+        if ((background[cur_state[0]+i, cur_state[1], 0] < 28) and (background[cur_state[0]+i, cur_state[1], 1] < 5) and (background[cur_state[0]+i, cur_state[1], 2] >130)):
+            print(background[cur_state[0]+i, cur_state[1], :])
+            return True
+        elif ((background[cur_state[0], cur_state[1]+i, 0] < 28) and (background[cur_state[0], cur_state[1]+i, 1] < 5) and (background[cur_state[0], cur_state[1]+i, 2] >130)):
+            print(background[cur_state[0], cur_state[1]+i, :])
+            return True
+        elif ((background[cur_state[0]-i, cur_state[1], 0] < 28) and (background[cur_state[0]-i, cur_state[1], 1] < 5) and (background[cur_state[0]-i, cur_state[1], 2] >130)):
+            print(background[cur_state[0]-i, cur_state[1], :])
+            return True
+        elif ((background[cur_state[0], cur_state[1]-i, 0] < 28) and (background[cur_state[0], cur_state[1]-i, 1] < 5) and (background[cur_state[0], cur_state[1]-i, 2] >130)):
+            print(background[cur_state[0], cur_state[1]-i, :])
+            return True
 
+        if sum(background[cur_state[0]+i, cur_state[1]]) < 20:
+            print(background[cur_state[0]+i, cur_state[1]])
+            return True
+        elif sum(background[cur_state[0], cur_state[1]+i]) < 20:
+            print(background[cur_state[0], cur_state[1]+i])
+            return True
+        elif sum(background[cur_state[0]-i, cur_state[1]]) < 20:
+            print(background[cur_state[0]-i, cur_state[1]])
+            return True
+        elif sum(background[cur_state[0], cur_state[1]-i]) < 20:
+            print(background[cur_state[0], cur_state[1]-i])
+            return True
 
-def is_win(cur_state, background):
-    if background[cur_state[0]+5, cur_state[1]+5, 1]>160 and (background[cur_state[0]+5, cur_state[1]+5, 0] <  90) and (background[cur_state[0]+5, cur_state[1]+5, 2] <  90):
-        return True
-    return False
-
-
-def update_state(state, cur_state, direction):
-    if direction == 'right':
-        cur_state[1] += 10
-    elif direction == 'left':
-        cur_state[1] -= 10
-    elif direction == 'up':
-        cur_state[0] -= 10
-    elif direction == 'down':
-        cur_state[0] +=10
-
-    state[cur_state[0]:cur_state[0]+10, cur_state[1]:cur_state[1]+10] = [255, 0, 0]
-
-
-def main():
-    background, maze = load_resources()
-    state, cur_state, background = init_game(background, maze)
-
-    start_time = time()
-
-    # init_background = state.copy()
-    while True:
-        cv.imshow( "Window", state)
-        k = cv.waitKey(1)
         
-        if k == 100: # right
-            update_state(state, cur_state, 'right')
-        elif k == 97: # left
-            update_state(state, cur_state, 'left')
-        elif k == 119: # up
-            update_state(state, cur_state, 'up')
-        elif k == 115: # down
-            update_state(state, cur_state, 'down')
-        elif k == 113 or k == 27:  # q or esc
-            break
-        if is_failed(cur_state, background, start_time):
-            # cv.putText(background, 'Failed!', org=(350, 420), color=(0, 0, 255), fontFace=cv.FONT_HERSHEY_DUPLEX, fontScale=5)
-            # display gameover image
-            failed = cv.imread('./resource/gameover.jpg')
-            cv.imshow("Window", failed)
+    if game_time > 60:
+        return True 
+        
+    return False
 
-            # cv.imshow("Window", background)
-            cv.waitKey(0)
-            break
-        if is_win(cur_state, background):
-            # cv.putText(background, 'You Win!', org=(350, 420), color=(0, 0, 255), fontFace=cv.FONT_HERSHEY_DUPLEX, fontScale=3)
-            # cv.imshow("Window", background)
-            win = cv.imread('./resource/win.jpg')
-            cv.imshow("Window", win)
-            cv.waitKey(0)
-            break
-
-        timer = np.zeros((50, 250, 3))
-
-        game_time = time() - start_time
-        cv.putText(timer, f'Time: {int(60-game_time%60):02}', org=(10, 30), color=(0, 0, 255), fontFace=cv.FONT_HERSHEY_SIMPLEX, fontScale=1)
-        state[0:50, 0:250] = timer
-
-    cv.destroyWindow("Window")
+def is_win(
+        cur_state, 
+        background
+        ):
+    win_area = 15
+    for i in range(win_area):
+        if background[cur_state[0]+i, cur_state[1], 1]>160 and (background[cur_state[0]+i, cur_state[1], 0] <  90) and (background[cur_state[0]+i, cur_state[1], 2] <  90):
+            print(background[cur_state[0]+i, cur_state[1], :])
+            return True
+        elif background[cur_state[0], cur_state[1]+i, 1]>160 and (background[cur_state[0], cur_state[1]+i, 0] <  90) and (background[cur_state[0], cur_state[1]+i, 2] <  90):
+            print(background[cur_state[0], cur_state[1]+i, :])
+            return True
+        elif background[cur_state[0]-i, cur_state[1], 1]>160 and (background[cur_state[0]-i, cur_state[1], 0] <  90) and (background[cur_state[0]-i, cur_state[1], 2] <  90):
+            print(background[cur_state[0]-i, cur_state[1], :])
+            return True
+        elif background[cur_state[0], cur_state[1]-i, 1]>160 and (background[cur_state[0], cur_state[1]-i, 0] <  90) and (background[cur_state[0], cur_state[1]-i, 2] <  90):
+            print(background[cur_state[0], cur_state[1]-i, :])
+            return True
+    
+    return False
 
 
-if __name__ == '__main__':
-    main()
+
+def update_state(
+        direction, 
+    ):
+    step = 15
+    if direction == 'right':
+        CUR_STATE[1] += step
+    elif direction == 'left':
+        CUR_STATE[1] -= step
+    elif direction == 'up':
+        CUR_STATE[0] -= step
+    elif direction == 'down':
+        CUR_STATE[0] += step
+
+    draw_player()
+
+
+# def main():
+start_time = time()
+
+while True:
+    cv.imshow( "Window", STATE)
+    k = cv.waitKey(100)
+    
+    if k == 100: # right
+        update_state('right')
+    elif k == 97: # left
+        update_state('left')
+    elif k == 119: # up
+        update_state('up')
+    elif k == 115: # down
+        update_state('down')
+    elif k == 113 or k == 27:  # q or esc
+        break
+
+    if is_failed(CUR_STATE, BACKGROUND, start_time):
+        print("Failed")
+        failed = cv.imread('./resource/gameover_2.jpg')
+        print(STATE.shape, failed.shape)
+        STATE = failed
+        cv.imshow("Window", STATE)
+
+        cv.waitKey(0)
+        break
+        
+
+    if is_win(CUR_STATE, BACKGROUND):
+        print("You Win")
+        win = cv.imread('./resource/win_2.jpg')
+        STATE = win
+        cv.imshow("Window", STATE)
+        cv.waitKey(0)
+        break
+        
+
+    timer = np.zeros((50, 250, 3))
+
+    game_time = time() - start_time
+    cv.putText(timer, f'Time: {int(60-game_time%60):02}', org=(10, 30), color=(0, 0, 255), fontFace=cv.FONT_HERSHEY_SIMPLEX, fontScale=1)
+    STATE[0:50, 0:250] = timer
+
+cv.destroyWindow("Window")
+
+
+# if __name__ == '__main__':
+#     main()
